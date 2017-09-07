6bef556b409d6f357eaf8fcc7b387418807229ad
John Stillwell <stillwell.john@gmail.com>
2017-09-07 12:56:14 -0400

    Added method to calculate the current season. Will work until the game is out of early access.

--- PUBGSharp/Helpers/Seasons.cs
+++ PUBGSharp/Helpers/Seasons.cs
@@ -1,4 +1,6 @@
-﻿namespace PUBGSharp.Helpers
+﻿using System;
+
+namespace PUBGSharp.Helpers
 {
     /// <summary>
     /// Defines all the season names provided by the API to be used with LINQ / IEnumerable selector functions.
@@ -8,5 +10,15 @@
         public const string EASeason1 = "2017-pre1";
         public const string EASeason2 = "2017-pre2";
         public const string EASeason3 = "2017-pre3";
+
+        public static string CurrentSeason() {
+            var season4StartDate = new DateTime(2017, 9, 1);
+            var today = DateTime.Now;
+            var monthsSinceSept2017 = ((today.Year - season4StartDate.Year) * 12) + Math.Abs(today.Month - season4StartDate.Month);
+
+            var season = 4 + monthsSinceSept2017;
+
+            return $"{today.Year}-pre{season}";
+        }
     }
 }
